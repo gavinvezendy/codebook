fa1acf6421ca2de981fc8b71f38acaa43c3155ac
Gavin <gavinvezendy@gmail.com>
2018-11-12 17:36:28 -0500

    Worked on making it bounce

--- platformer/game.js
+++ platformer/game.js
@@ -1,12 +1,24 @@
 new Phaser.Game({
     width: 683, // half of 1386
     height: 384, // half of 786
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300, x: 100},
+            debug: true
+        },
+    },
     scene: {
         preload: function () {
-            this.load.image('bg', '././assets/plat-background.png')
+            this.load.image('bg', './assets/plat-background.png')
+            this.load.image('hi', './assets/app-person.png')
         },
         create() {
             this.add.image(0, 0, 'bg').setOrigin(0,0)
+            player = this.physics.add.sprite(150, 150, 'hi')
+            player.setCollideWorldBounds(true)
+            player.setBounce(1)  // 0 to 1
         }
+        
     }
-})
+}) 
