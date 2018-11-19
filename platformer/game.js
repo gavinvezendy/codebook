05755dbc7008ea08acd013dac8da69610c60a1ca
Gavin <gavinvezendy@gmail.com>
2018-11-19 17:40:21 -0500

    Made the player move using WASD

--- platformer/game.js
+++ platformer/game.js
@@ -1,10 +1,12 @@
+let player, curs
+
 new Phaser.Game({
     width: 683, // half of 1386
     height: 384, // half of 786
     physics: {
         default: 'arcade',
         arcade: {
-            gravity: { y: 300, x: 100},
+            gravity: { y: 1200,},
             debug: true
         },
     },
@@ -17,8 +19,22 @@ new Phaser.Game({
             this.add.image(0, 0, 'bg').setOrigin(0,0)
             player = this.physics.add.sprite(150, 150, 'hi')
             player.setCollideWorldBounds(true)
-            player.setBounce(1)  // 0 to 1
-        }
+            player.setBounce(.2)  // 0 to 1
         
+            curs = this.input.keyboard.createCursorKeys()
+        },
+        
+        update() {
+
+            if (curs.left.isDown) {
+                player.setVelocityX(-200)
+               }   else if (curs.right.isDown) {
+                    player.setVelocityX(200)
+                }
+            }
+
+        
+        }
+
     }
-}) 
+) 
