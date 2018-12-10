13299287f00af193709bd929e79154284586525f
Gavin <gavinvezendy@gmail.com>
2018-12-10 18:01:21 -0500

    finished

---
+++ platformer/README.md
@@ -0,0 +1,15 @@
+# Notes for Phaser3
+
+* https://labs.phaser.io/
+* https://phaser.io
+* https://skilstak.io/games
+
+Don't forget you can edit on GitHub also.
+
+## Assets
+
+* 683x384 background
+* 32x32 player
+* 100x30 platform
+* 16x16 coin or something to pick up
+* 32x32 bad guy 
--- platformer/game.js
+++ platformer/game.js
@@ -5,6 +5,10 @@ let pl, k, plats
 function preload() {
     this.load.image('bg', './assets/plat-background.png')
     this.load.image('hi', './assets/app-person.png')
+    this.load.image('bg', './assets/boss-ant-badguy.png')
+    this.load.image('gb', './assets/coin.png')
+    this.load.image('ih', './assets/platform.png')
+
 }
 
 function create() {
@@ -12,9 +16,17 @@ function create() {
     
     pl = this.physics.add.sprite(150, 150, 'hi')
     pl.setCollideWorldBounds(true)
-    pl.setScale(5.5)
-    pl.setBounce(.2)  // 0 to 1
-    pl.setDragX(1300)
+    pl.setScale(2)
+    pl.setBounce(.13)  // 0 to 1
+    pl.setDragX(1200)
+
+    plats = this.physics.add.staticGroup()
+    plats.create(300,300, 'ih')
+
+    plats.create(200,200, 'gb')
+
+    this.physics.add.collider(pl,plats)
+    
 
     k = this.input.keyboard.addKeys(keys)
 }
@@ -30,7 +42,7 @@ function update() {
     if (pl.body.onFloor()) {
         
         if (k.UP.isDown || k.W.isDown) {
-            pl.setVelocityY(-400)
+            pl.setVelocityY(-515)
         }
         
     }
